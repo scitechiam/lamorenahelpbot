1a55b6bbcb3713bc2bd7cff419ff58d1700aee13
scitechiam <scitech.iam@gmail.com>
2024-07-31 13:33:08 -0500

    added main page

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-const mongodb = require("./database/connection.js");
+// app.js
+const express = require('express');
+const path = require('path');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware para servir archivos estáticos
+app.use(express.static(path.join(__dirname, 'client')));
+
+// Ruta para servir index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
